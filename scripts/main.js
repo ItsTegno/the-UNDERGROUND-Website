7c34720e6f8430fbf941b1c0b8d00667ae5789d2
ItsTegno <davidmunozsegura8@gmail.com>
2025-09-28 19:08:10 +0200

    Ala luego sigo un rato mas

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,34 @@
 window.addEventListener('scroll', function() {
-    const h1 = document.querySelector('h1');
     const scrollY = window.scrollY;
-    const maxScroll = window.innerHeight * 0.9; // Ajusta según lo que necesites
+    const maxScroll = window.innerHeight * 1; // Ajusta según lo que necesites
 
     // Progreso de 0 a 1
     const progress = Math.min(scrollY / maxScroll, 1);
 
-    // Controla el avance de la animación
-    h1.style.animationPlayState = 'paused';
-    h1.style.animationDelay = `-${progress}s`;
-
     // Desenfoque dinámico del fondo
     const bg = document.getElementById('background_blur');
+
     if (bg) {
         const blurAmount = progress * 10; // 0px a 10px de blur
         bg.style.filter = `blur(${blurAmount}px)`;
 
-        const parallax = scrollY * -0.4; // Ajusta el factor para más/menos efecto
+        const parallax = scrollY * -0.5; // Ajusta el factor para más/menos efecto
         bg.style.transform = `translateY(${parallax}px)`;
     }
 });
+
+window.addEventListener("load", () => {
+      document.body.classList.add("loaded");
+    });
+
+    // Al salir, hacemos fade-out
+    document.querySelectorAll("a").forEach(link => {
+      link.addEventListener("click", e => {
+        e.preventDefault();
+        const url = link.href;
+        document.body.classList.remove("loaded");
+        setTimeout(() => {
+          window.location.href = url;
+        }, 800); // mismo tiempo que el transition
+      });
+    });
